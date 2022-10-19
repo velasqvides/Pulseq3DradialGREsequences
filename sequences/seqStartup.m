eaaaace37eb5038dbebf090a0cdd4daed1689bda
velasqvides <velasqvides@gmail.com>
2022-10-19 11:12:12 +0200

    add extra folder for tested sequences
    This was done to test the version v1.0 of the project

--- sequences/seqStartup.m
+++ sequences/seqStartup.m
@@ -1,6 +1,6 @@
 % set the two matlab paths for the creation of sequences
 % pulseq setup 
-pulseqPath = '~/Documents/tools/pulseq/matlab';
+pulseqPath = 'C:\Users\jvelasq\Documents\GitHub\pulseq\matlab';
 addpath(genpath(pulseqPath));
 % radial_GRE_sequences setup
 radialPath = fullfile(pwd, 'classes_and_functions');
